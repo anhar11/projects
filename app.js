dc61192806b057ba76b8dc9b6e58c24f2d1e2622
tomerbu <bu.tomer@gmail.com>
2017-01-24 13:49:26 +0200

    added initdb route

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(function(req, res, next){
+  req.time = new Date();
+  //pasportJS
+  next();
+});
+
+
 app.use('/', index);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+router.get('/initdb', function (req, res, next) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    if (err) {
+      return next(err);
+    }
+
+    client.query(`CREATE TABLE Students(id SERIAL PRIMARY KEY,
+                                        firstName TEXT NOT NULL,
+                                        lastName TEXT NOT NULL,
+                                        email TEXT NOT NULL)`,
+
+      /*callback*/
+      function (err, result) {
+        done();
+        if (err) {
+          return next(err);
+        }
+
+        res.render('index', {
+          title: "DB Initialized!"
+        });
+      }
+    );
+  });
+});
+
+router.get('/addStudent', function (req, res, next) {
+
+});
+
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
